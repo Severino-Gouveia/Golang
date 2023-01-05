5e24c27ecede861331304edb7baad72eab61749e
severino gouveia <severinogouveia_@hotmail.com>
2023-01-05 15:40:53 -0300

    Desafio Ping e Pong

--- .vscode/.vscode/desafios de codigos/Desafio Ping e Pang/Desafio Ping e Pang.go
+++ .vscode/.vscode/desafios de codigos/Desafio Ping e Pang/Desafio Ping e Pang.go
@@ -6,7 +6,7 @@ func main() {
 
 	for i := 1; i <= 100; i++ {
 		if i%3 == 0 && i%5 == 0 {
-			fmt.Println("PingPang")
+			fmt.Println("Ping Pang")
 		} else if i%3 == 0 {
 			fmt.Println("Ping")
 		} else if i%5 == 0 {
